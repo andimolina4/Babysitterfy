de8406ec086919c0588f0012a9e2b8dc520f917d
magaliamedina <magalimedina96@gmail.com>
2022-07-18 09:20:49 -0300

    pruebaa :)

--- LucasTurza/BabysitterfyApp/Models/BabySitter.cs
+++ LucasTurza/BabysitterfyApp/Models/BabySitter.cs
@@ -4,6 +4,7 @@ namespace BabysitterfyApp.Models
 {
     public class BabySitter
     {
+        //:)
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
